adc83f0c74c031154e60290b904049d27ee53705
Naim Miah <mmiah@law.fordham.edu>
2025-03-17 02:28:35 -0400

    last bit edits to favicon

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,15 @@ import { NextConfig } from "next/dist/types";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Add this webpack configuration
+
   webpack: (config, { isServer }) => {
-    // If we're building for the browser (not server)
     if (!isServer) {
-      // Replace Node.js modules with empty objects
       config.resolve.fallback = {
         ...config.resolve.fallback,
         fs: false,
         path: false,
         os: false,
         crypto: false,
-        // Add any other Node.js built-ins that are causing issues
       };
     }
     return config;
